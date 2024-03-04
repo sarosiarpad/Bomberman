d38baaceedfecbec0913a8fb7b6861c36a81ce62
Sarosi Arpad <lout5f@inf.elte.hu>
2024-03-04 23:45:54 +0100

    Kezdetleges mapgenerálás txt fájlból

--- core/src/com/mygdx/game/BombermanGame.java
+++ core/src/com/mygdx/game/BombermanGame.java
@@ -5,56 +5,39 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BombermanGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	Wall[] walls;
-	Box[] boxes;
+	List<Wall> walls = new ArrayList<>();
+	List<Box> boxes = new ArrayList<>();
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-
-		// Falak inicializálása
-		walls = new Wall[3];
-		walls[0] = new Wall(100, 150);
-		walls[1] = new Wall(150, 150);
-		walls[2] = new Wall(150, 200);
-
-		// Dobozok inicializálása
-		boxes = new Box[3];
-		boxes[0] = new Box(100, 200);
-		boxes[1] = new Box(200, 300);
-		boxes[2] = new Box(300, 400);
+		try {
+			Map.generateMap(walls, boxes);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 0);
 		batch.begin();
 
-		for (Wall wall : walls) {
-			batch.draw(
-					wall.getTexture(),
-					wall.getX(),
-					wall.getY(),
-					wall.getWidth(),
-					wall.getHeight()
-			);
+		for(Wall wall : walls){
+			batch.draw(wall.getTexture(), wall.getX(), wall.getY(), wall.getWidth(), wall.getHeight());
+		}
+		for(Box box : boxes){
+			batch.draw(box.getTexture(), box.getX(), box.getY(), box.getWidth(), box.getHeight());
 		}
 
-		// Dobozok megjelenítése
-		for (Box box : boxes) {
-			batch.draw(
-					box.getTexture(),
-					box.getX(),
-					box.getY(),
-					box.getWidth(),
-					box.getHeight()
-			);
-		}
-
-		batch.end();
+        batch.end();
 	}
 	
 	@Override
---
+++ core/src/com/mygdx/game/Map.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Map {
+
+    public static void generateMap(List<Wall> walls, List<Box> boxes) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("assets/maps/map1.txt"));
+        String line;
+        int x = 0;
+        while ((line = reader.readLine()) != null) {
+            String[] tiles = line.split("");
+            int y = 0;
+            for (String tile : tiles) {
+                switch (tile) {
+                    case "1":
+                        walls.add(new Wall(x * 50, y * 50));
+                        break;
+                    case "2":
+                        boxes.add(new Box(x * 50, y * 50));
+                        break;
+                    default:
+                        // Ha az érték nem 1 vagy 2, nincs semmi teendő
+                        break;
+                }
+                y++;
+            }
+            x++;
+        }
+        reader.close();
+    }
+}
