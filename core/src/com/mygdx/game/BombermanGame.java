436ea668d36f5ecb257cb643870a293cfc76e5ac
Bálits Péter Márton <hfyqyw@inf.elte.hu>
2024-03-11 12:40:27 +0000

    Map refaktorálása, Player bővítése: Sprite + Input

--- core/src/com/mygdx/game/BombermanGame.java
+++ core/src/com/mygdx/game/BombermanGame.java
@@ -11,16 +11,25 @@ import java.util.List;
 
 public class BombermanGame extends ApplicationAdapter {
 	SpriteBatch batch;
+	Map map = new Map("assets/maps/map1.txt");
 	List<Wall> walls = new ArrayList<>();
 	List<Box> boxes = new ArrayList<>();
+	List<Player> players = new ArrayList<>();
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+
+		int[][] raw = map.getRaw();
+
 		try {
-			Map.generateMap(walls, boxes);
+			map.generate(walls, boxes);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
+		}
+
+		for (int i = 0; i < 1; ++i) {
+			players.add(new Player(10,1,null, raw));
 		}
 	}
 
@@ -34,6 +43,9 @@ public class BombermanGame extends ApplicationAdapter {
 		}
 		for(Box box : boxes){
 			batch.draw(box.getTexture(), box.getX(), box.getY(), box.getWidth(), box.getHeight());
+		}
+		for( Player player : players) {
+			batch.draw(player.getTexture(), player.getX(), player.getY(), player.getWidth(), player.getHeight());
 		}
 
         batch.end();
